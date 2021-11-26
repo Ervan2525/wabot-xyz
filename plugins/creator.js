b0970c7b2b78e8eb9551c524a22cc507e2eb0f78
Jayaganz <92702704+Jayaganz@users.noreply.github.com>
2021-11-26 09:44:04 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,4 @@
 function handler(m) {
-  // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
   this.sendContact(m.chat, '6281334177340', 'Nurutomo', m)
 }
 handler.help = ['owner', 'creator']
