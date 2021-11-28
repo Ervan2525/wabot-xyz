f37887db784709a090eec5cdf1fb7c7f974be2d4
Jayaganz <92702704+Jayaganz@users.noreply.github.com>
2021-11-29 03:28:52 +0800

    Update tebakbendera_answer.js

--- plugins/tebakbendera_answer.js
+++ plugins/tebakbendera_answer.js
@@ -7,12 +7,12 @@ handler.before = async function (m) {
   if (m.quoted.id == conn.tebakbendera[id][0].id) {
     let json = JSON.parse(JSON.stringify(conn.tebakbendera[id][1]))
     // m.reply(JSON.stringify(json, null, '\t'))
-    if (m.text.toLowerCase() == json.jawaban.toLowerCase()) {
+    if (m.text.toLowerCase() == json.name.toLowerCase()) {
       global.DATABASE._data.users[m.sender].uang += conn.tebakbendera[id][2]
       m.reply(`*Benar!*\n+Rp${conn.tebakbendera[id][2]}`)
       clearTimeout(conn.tebakbendera[id][3])
       delete conn.tebakbendera[id]
-    } else if (m.text.toLowerCase().endsWith(json.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
+    } else if (m.text.toLowerCase().endsWith(json.name.split` `[1])) m.reply(`*Dikit Lagi!*`)
     else m.reply(`*Salah!*`)
   }
 }
