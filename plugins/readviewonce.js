e7b83f7ed90ded73554fe5a568445639a6088c16
Jayaganz <92702704+Jayaganz@users.noreply.github.com>
2021-11-28 11:55:08 +0800

    Update readviewonce.js

--- plugins/readviewonce.js
+++ plugins/readviewonce.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
     await conn.copyNForward(m.chat, await conn.loadMessage(m.chat, m.quoted.id), false, { readViewOnce: true }).catch(_ => m.reply('Mungkin dah pernah dibuka bot'))
 }
 
-handler.help = ['readviewonce']
+handler.help = ['readviewonce', 'read', 'baca']
 handler.tags = ['tools']
 
 handler.command = /^readviewonce/i
