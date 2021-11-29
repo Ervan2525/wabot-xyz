d65e6789f5f9d0fca4798acb14077a534d83fb20
Jayaganz <92702704+Jayaganz@users.noreply.github.com>
2021-11-30 05:04:55 +0800

    Update pinterest2.js

--- plugins/pinterest2.js
+++ plugins/pinterest2.js
@@ -5,16 +5,14 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     m.reply(global.wait)
     if (!res.ok) throw await res.text()
     let json = await res.json()
-    // let img = `${json.image}`
-    let itsuki = `
+    let img = `${json.image}`
+    let caption = `
 Pinterest!
 Query: ${text}
 `.trim()
-await conn.sendButtonImg(m.chat, await (await fetch(json.image)).buffer(), itsuki, '© Haruno', '🔎CARI LAGI', `.pinterest ${text}`, { thumbnail: Buffer.alloc(0) })
+await conn.sendFile(m.chat, img, 'pinterest2.jpg', caption, m)
 }
-handler.command = /^pinterest$/i
+handler.command = /^pinterest2$/i
 handler.tags = ['internet']
-handler.help = ['pinterest <query>']
+handler.help = ['pinterest2 <query>']
 module.exports = handler
-//Tidak work untuk pesan sementara
-//©Haruno
