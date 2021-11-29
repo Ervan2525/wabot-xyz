64b65052e7ca00ad316cb2c7dc78a7fb2d822df3
Jayaganz <92702704+Jayaganz@users.noreply.github.com>
2021-11-29 08:11:04 +0800

    Update tebakkimia_ans.js

--- plugins/tebakkimia_ans.js
+++ plugins/tebakkimia_ans.js
@@ -9,12 +9,12 @@ module.exports = {
         if (m.quoted.id == this.tebakkimia[id][0].id) {
             let json = JSON.parse(JSON.stringify(this.tebakkimia[id][1]))
             // m.reply(JSON.stringify(json, null, '\t'))
-            if (m.text.toLowerCase() == json.name.toLowerCase().trim()) {
+            if (m.text.toLowerCase() == json.unsur.toLowerCase().trim()) {
                 global.db.data.users[m.sender].exp += this.tebakkimia[id][2]
                 m.reply(`*Benar!*\n+${this.tebakkimia[id][2]} XP`)
                 clearTimeout(this.tebakkimia[id][3])
                 delete this.tebakkimia[id]
-            } else if (similarity(m.text.toLowerCase(), json.name.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
+            } else if (similarity(m.text.toLowerCase(), json.unsur.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
             else m.reply(`*Salah!*`)
         }
         return !0
