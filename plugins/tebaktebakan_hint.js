f2a40faf03ed1ff740b467964acf800f03c0cbbe
Jayaganz <92702704+Jayaganz@users.noreply.github.com>
2021-11-29 08:31:09 +0800

    Update tebaktebakan_hint.js

--- plugins/tebaktebakan_hint.js
+++ plugins/tebaktebakan_hint.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
     let id = m.chat
     if (!(id in conn.tebaktebakan)) throw false
     let json = conn.tebaktebakan[id][1]
-    let nya = json.result.jawaban
+    let nya = json.jawaban
     let nyanya = nya.replace(/[bcdfghjklmnpqrstvwxyz]/g, '_')
     m.reply('```' + nyanya + '```')
 }
