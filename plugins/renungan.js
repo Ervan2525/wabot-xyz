9437aa7a3405a7e8526084a715f23fe24951fb52
Jayaganz <92702704+Jayaganz@users.noreply.github.com>
2021-11-29 08:57:04 +0800

    Create renungan.js

---
+++ plugins/renungan.js
@@ -0,0 +1,16 @@
+let handler = async(m, { conn }) => {
+  let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/kata-kata/renungan.json')).json()
+  let json = src[Math.floor(Math.random() * src.length)]
+  await conn.sendFile(m.chat, json, 'renungan.jpeg', 'Sukses!', m)
+}
+handler.help = ['renungan']
+handler.tags = ['maker']
+handler.command = /^(renungan)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.register = true
+handler.fail = null
+
+handler.limit = true
+module.exports = handler
