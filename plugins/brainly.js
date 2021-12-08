3a6ee798d14d91cd29db8f002cd73f9ae4cd0e20
Jayaganz <92702704+Jayaganz@users.noreply.github.com>
2021-12-08 18:33:14 +0800

    Update brainly.js

--- plugins/brainly.js
+++ plugins/brainly.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async function (m, { text }) {
   if (!text) throw 'Soalnya?'
-  let res = await fetch(global.APIKeys('xteam', 'brainly', { soal=text }, 'APIKEY'))
+  let res = await fetch(global.APIs('xteam', 'brainly', { soal=text }, 'APIKEY'))
   m.reply(res.jawaban)
 }
 handler.help = ['brainly <soal>']
