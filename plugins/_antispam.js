ae1e0e75bafa42ebc4f672aeb1b151fcd23d4272
Jayaganz <92702704+Jayaganz@users.noreply.github.com>
2021-12-08 15:53:25 +0800

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -6,14 +6,8 @@ module.exports = {
             this.spam[m.sender].count++
             if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 5) {
                 if (this.spam[m.sender].count > 5) {
-                    let log = {
-                      key: m.key,
-                      count: 5,
-                      lastspam: 5,
-                      sender: m.sender
-                    }
                     //global.db.data.users[m.sender].banned = true
-                    m.reply('*Spam Terdeteksi!!*' + require(util).format(log))
+                    m.reply('*JANGAN SPAM!*')
                     this.reply(global.owner[0] + '@s.whatsapp.net', `*Spam dideteksi oleh bot!*\n\nSender: @${m.sender.split`@`[0]}\nChat: ${m.isGroup ? m.chat : m.sender}\nName sender: ${m.isGroup ? this.getName(m.chat) : this.getName(m.sender)}`.trim(), null, { contextInfo: { mentionedJid: [m.sender] }})
                 }
                 this.spam[m.sender].count = 0
