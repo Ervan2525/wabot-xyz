c782a8a359fe26f4d1b8016e0cacf1e0317aac21
Jayaganz <92702704+Jayaganz@users.noreply.github.com>
2021-12-08 15:36:22 +0800

    Create tebakanime_hint.js

---
+++ plugins/tebakanime_hint.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn }) => {
+    conn.tebakanime = conn.tebakanime ? conn.tebakanime : {}
+    let id = m.chat
+    if (!(id in conn.tebakanime)) throw false
+    let json = conn.tebakanime[id][1]
+    let nya = json.name
+    let nyanya = nya.replace(/[bcdfghjklmnpqrstvwxyz]/g, '_')
+    m.reply('```' + nyanya + '```')
+}
+handler.command = /^ano$/i
+
+module.exports = handler
