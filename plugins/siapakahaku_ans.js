967906f8f2af667749ad66d23e6bd1c3ffce726e
Jayaganz <92702704+Jayaganz@users.noreply.github.com>
2021-12-08 13:20:31 +0800

    Update siapakahaku_ans.js

--- plugins/siapakahaku_ans.js
+++ plugins/siapakahaku_ans.js
@@ -9,13 +9,13 @@ handler.before = async function (m) {
     if (m.quoted.id == this.siapakahaku[id][0].id) {
         let json = JSON.parse(JSON.stringify(this.siapakahaku[id][1]))
         // m.reply(JSON.stringify(json, null, '\t'))
-        if (m.text.toLowerCase() == json.result.jawaban.toLowerCase().trim()) {
+        if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             global.db.data.users[m.sender].
                 exp += this.siapakahaku[id][2]
             m.reply(`*Benar!*\n+${this.siapakahaku[id][2]} XP`)
             clearTimeout(this.siapakahaku[id][3])
             delete this.siapakahaku[id]
-        } else if (similarity(m.text.toLowerCase(), json.result.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
+        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
         else m.reply(`*Salah!*`)
     }
     return !0
