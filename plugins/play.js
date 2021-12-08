e14645307c43450de1630635e997a028e0d21fd5
Jayaganz <92702704+Jayaganz@users.noreply.github.com>
2021-12-08 20:00:32 +0800

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -53,3 +53,7 @@ handler.limit = true
 
 module.exports = handler
 
+async function shortlink(url) {
+isurl = /https?:\/\//.test(url)
+return isurl ? (await require('axios').get('https://tinyurl.com/api-create.php?url='+encodeURIComponent(url))).data : ''
+}
