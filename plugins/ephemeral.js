7a2d9424b4a218efae1bd48e7b383c747e6f36c3
Jayaganz <92702704+Jayaganz@users.noreply.github.com>
2021-12-13 04:55:58 +0800

    Update ephemeral.js

--- plugins/ephemeral.js
+++ plugins/ephemeral.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, args, isBotAdmin, isAdmin, isOwner }) => {
+let handler = async (m, { conn, args, isBotAdmin, isAdmin, isOwner, usedPrefix }) => {
     if (m.isGroup) {
         if (!isBotAdmin) {
             global.dfail('botAdmin', m, conn)
@@ -9,13 +9,22 @@ let handler = async (m, { conn, args, isBotAdmin, isAdmin, isOwner }) => {
             throw false
         }
     }
-    if (args[0] == 'on') await conn.toggleDisappearingMessages(
+    m.reply(`*Pilihan tersedia untuk pesan sementara:*\n\n1. 24 jam: ${usedPrefix}ephe 1\n2. 7 hari: ${usedPrefix}ephe 7\n3. 90 hari: ${usedPrefix}ephe 90
+    if (args[0] == '1') await conn.toggleDisappearingMessages(
+        m.chat,
+        1 * 24 * 60 * 60
+    )
+    if (args[0] == '7') await conn.toggleDisappearingMessages(
         m.chat,
         7 * 24 * 60 * 60
     )
+    if (args[0] === '90' await conn.toggleDisappearingMessages(
+        m.chat,
+        90 * 24 * 60 * 60
+    )
     else await conn.toggleDisappearingMessages(m.chat, 0)
 }
-handler.help = ['ephe [on]']
+handler.help = ['ephe [1, 7, 90]']
 handler.tags = ['tools']
 handler.command = /^(ephe)$/i
 
