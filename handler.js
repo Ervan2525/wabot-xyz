999c5164f185bd12f7d7f5b881bb24c62c3ec75c
Jayaganz <92702704+Jayaganz@users.noreply.github.com>
2021-12-13 04:43:32 +0800

    Update handler.js

--- handler.js
+++ handler.js
@@ -363,7 +363,7 @@ module.exports = {
                 .setBackground("./src/background.png")
                 .toAttachment()
 
-              this.sendButtonLoc(jid, action === 'add' ? wel.toBuffer() : lea.toBuffer(), text, 'Whatsapp Bot', action === 'add' ? 'SELAMAT DATANG!', `${usedPrefi}say Disapa dulu dong member barunya suruh intro dulu :)` : 'SELAMAT TINGGAL!', `${usedPrefix}say Semoga dia tenang di alam sana ya!`, null, false, {
+              this.sendButtonLoc(jid, action === 'add' ? wel.toBuffer() : lea.toBuffer(), text, 'Whatsapp Bot', action === 'add' ? 'SELAMAT DATANG!', `${usedPrefix}say Disapa dulu dong member barunya suruh intro dulu :)` : 'SELAMAT TINGGAL!', `${usedPrefix}say Semoga dia tenang di alam sana ya!`, null, false, {
                 contextInfo: {
                   mentionedJid: [user]
                 }
