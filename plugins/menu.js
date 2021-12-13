8541440776c989254f3ec5c238876a807c3705b8
Jayaganz <92702704+Jayaganz@users.noreply.github.com>
2021-12-13 10:31:58 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -162,7 +162,8 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    conn.reply(m.chat, text.trim(), m)
+    //conn.reply(m.chat, text.trim(), m)
+    conn.sendFile(m.chat, fs.readFileSync('./src/logo.jpg'), 'logo.jpg', text.trim(), m)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
